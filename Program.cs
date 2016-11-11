8c8cbf17f0973acee940eee7561e45dff6c27ac7
Maurits van der Schee <maurits@vdschee.nl>
2016-11-11 23:10:19 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+            .UseUrls("http://localhost:8000/")
+            .UseKestrel()
+            .UseStartup<Startup>()
+            .Build();
+ 
+            host.Run();
+        }
+    }
+
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app){
+            app.Run(this.handler);
+        }
+
+        public Task handler(HttpContext context) {
+           //Console.WriteLine("Received request");
+           return context.Response.WriteAsync("Hello world");
+        }
+    }
+}
